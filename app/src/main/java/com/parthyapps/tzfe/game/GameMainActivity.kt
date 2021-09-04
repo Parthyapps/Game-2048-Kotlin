ec8ebd87047724bb6e2cb5b36f11d5820f36edf5
parthiban <parthiban@divum.in>
2021-09-04 22:54:48 +0530

    Initial commit

---
+++ app/src/main/java/com/parthyapps/tzfe/game/GameMainActivity.kt
@@ -0,0 +1,11 @@
+package com.parthyapps.tzfe.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_main)
+    }
+}
