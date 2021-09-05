c511a5ff7e3a90773fa58edd75c3a256680de421
parthiban <parthiban@divum.in>
2021-09-05 11:53:58 +0530

    base setup done grid layout design with textview and reset button done

--- app/src/main/java/com/parthyapps/tzfe/game/utils/Constants.kt
+++ app/src/main/java/com/parthyapps/tzfe/game/utils/Constants.kt
@@ -1,8 +1,7 @@
 package com.parthyapps.tzfe.game.utils
 
 import android.graphics.Color
-import android.provider.FontRequest
-import android.support.annotation.FontRes
+import android.os.Build
 
 class Constants {
     companion object {
@@ -42,31 +41,9 @@ class Constants {
         // Random tile selection ratio
         const val RANDOM_RATIO = 70  // favours '2's over '4's by ~3.5:1 ~70%
 
-        // Audio files
-        const val HORRAY_AUDIO_FN = "cheer.mp3"        // A MP# FILE
-        const val SAD_AUDIO_FN = "negative-beeps.wav"  // A WAV FILE
-        const val MP3_AUDIO = "mp3"                    // A WAV FILE
-        const val WAV_AUDIO = "wav"                    // A WAV FILE
-        const val AUDIO_SUBDIRECTORY = "sound.assets/"
-
         // Images
         //val SOUND_ON_IMG = #imageLiteral(resourceName: "sound_on")
         //val SOUND_OFF_IMG = #imageLiteral(resourceName: "sound_off")
 
-        // All the tile colours
-        val GAME_BOARD_COLOUR = Color.valueOf(187.0f / 255, 173.0f / 255, 160.0f / 255, 1.0f)
-        val TILE_BG_COLOUR = Color.valueOf(204.0f / 255, 192.0f / 255, 179.0f / 255f, 1.0f)
-        val TILE2_COLOUR = Color.valueOf(238.0f / 255, 228.0f / 255, 244.0f / 255, 1.0f)
-        val TILE4_COLOUR = Color.valueOf(237.0f / 255, 224.0f / 255, 200.0f / 255, 1.0f)
-        val TILE8_COLOUR = Color.valueOf(245.0f / 255, 149.0f / 255, 99.0f / 255, 1.0f)
-        val TILE16_COLOUR = Color.valueOf(245.0f / 255, 130.0f / 255, 97.0f / 255, 1.0f)
-        val TILE32_COLOUR = Color.valueOf(246.0f / 255, 124.0f / 255, 95.0f / 255, 1.0f)
-        val TILE64_COLOUR = Color.valueOf(246.0f / 255, 94.0f / 255, 59.0f / 255, 1.0f)
-        val TILE128_COLOUR = Color.valueOf(237.0f / 255, 207.0f / 255, 114.0f / 255, 1.0f)
-        val TILE256_COLOUR = Color.valueOf(237.0f / 255, 207.0f / 255, 114.0f / 255, 1.0f)
-        val TILE512_COLOUR = Color.valueOf(237.0f / 255, 200.0f / 255, 80.0f / 255, 1.0f)
-        val TILE1024_COLOUR = Color.valueOf(237.0f / 255, 197.0f / 255, 63.0f / 255, 1.0f)
-        val TILE2048_COLOUR = Color.valueOf(237.0f / 255, 194.0f / 255, 46.0f / 255, 1.0f)
-        val DEFAULT_COLOUR = Color.DKGRAY
     }
 }
--- app/src/main/java/com/parthyapps/tzfe/game/view/GameMainActivity.kt
+++ app/src/main/java/com/parthyapps/tzfe/game/view/GameMainActivity.kt
@@ -1,11 +1,16 @@
-package com.parthyapps.tzfe.game
+package com.parthyapps.tzfe.game.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.parthyapps.tzfe.game.R
+import com.parthyapps.tzfe.game.databinding.ActivityGameMainBinding
 
 class GameMainActivity : AppCompatActivity() {
+
+    private var activityGameMainBinding: ActivityGameMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_main)
+        activityGameMainBinding = ActivityGameMainBinding.inflate(layoutInflater)
     }
 }
