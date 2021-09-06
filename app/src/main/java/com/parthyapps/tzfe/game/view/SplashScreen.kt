1abf866eb57bc8a1d2f0674d77fe47be91ab68f9
parthiban <parthiban@divum.in>
2021-09-06 16:15:10 +0530

    git splash screen icon added

--- app/src/main/java/com/parthyapps/tzfe/game/view/SplashScreen.kt
+++ app/src/main/java/com/parthyapps/tzfe/game/view/SplashScreen.kt
@@ -1,25 +1,28 @@
 package com.parthyapps.tzfe.game.view
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.os.Handler
+import android.os.Looper
 import android.view.View
 import com.parthyapps.tzfe.game.databinding.ActivityGameMainBinding
 import com.parthyapps.tzfe.game.databinding.ActivitySplashBinding
 import com.parthyapps.tzfe.game.view.GameMainActivity
 
-class SplashScreen : AppCompatActivity() {
+@SuppressLint("CustomSplashScreen")
+open class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val activitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         val view: View = activitySplashBinding.root
         setContentView(view)
-        Handler().postDelayed({
-            val mainIntent = Intent(this@SplashScreen, GameMainActivity::class.java)
-            startActivity(mainIntent)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this@SplashScreen, GameMainActivity::class.java)
+            startActivity(i)
             finish()
-        }, 2000)
+        }, 3000)
     }
 }
